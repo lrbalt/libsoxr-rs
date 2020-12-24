ceea8dc67b7f026a8a692e65e561188654434a1a
Reinier Balt <lrbalt@gmail.com>
2020-12-24 15:00:10 +0100

    prepare for release on crates.io

---
+++ docs/implementors/core/marker/trait.StructuralEq.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["libsoxr"] = [{"text":"impl StructuralEq for Datatype","synthetic":false,"types":[]},{"text":"impl StructuralEq for QualityFlags","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ docs/implementors/core/marker/trait.StructuralPartialEq.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["libsoxr"] = [{"text":"impl StructuralPartialEq for Datatype","synthetic":false,"types":[]},{"text":"impl StructuralPartialEq for QualityFlags","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
